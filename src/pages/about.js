9957cfad1f15abfecf83955f2e12106992e9ecfe
Ashley King <ashleyot@gmail.com>
2019-05-06 21:02:23 -0400

    changed file name

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection";
-import Info from '../components/Home/Info'
+import Info from '../components/Home/info'
 
 const AboutPage = ({data}) => (
   <Layout>
